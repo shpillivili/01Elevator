823315f4319d4dfa37a4448b20c10f5ac55788dc
shpillivili <92676901+shpillivili@users.noreply.github.com>
2021-10-17 20:32:25 +0300

    Initial commit

---
+++ src/Elevator.java
@@ -0,0 +1,41 @@
+public class Elevator {
+
+    private int currentFloor = 1; //текущий этаж
+    private int minFloor = 0; //нижний этаж
+    private int maxFloor = 0; //верхний этаж
+
+    // конструктор, сохраняющий указанные параметры в переменных класса
+    public Elevator (int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+    // метод, перемещающий лифт на заданный в параметре этаж
+    public void move(int floor) {
+        while (getCurrentFloor() != floor) {
+            if (getCurrentFloor() < floor) {
+                moveUp();
+            }
+            else {
+                moveDown();
+            }
+            if (floor > maxFloor || floor < minFloor) {
+                System.out.println("Error");
+                break;
+            }
+            System.out.println(getCurrentFloor());
+        }
+    }
+    // метод, перемещающий лифт на один этаж вниз
+    public void moveDown () {
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+    }
+    // метод, перемещающий лифт на один этаж вверх
+    public void moveUp () {
+        currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor;
+    }
+    // метод, возвращающий текущий этаж, на котором находится лифт
+    public int getCurrentFloor () {
+        return currentFloor;
+    }
+
+}
